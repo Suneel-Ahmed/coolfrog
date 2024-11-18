c3c0804afc2ab2e45986f7587a820f60653cbfe2
Suneel-Ahmed <suneelahmed413@gmail.com>
2024-11-18 21:32:25 +0500

    add Project to github

---
+++ src/config/level-config.ts
@@ -0,0 +1,27 @@
+const levelConfig = {
+  frogs: {
+    1: "/images/logo/logo.png",
+    2: "/images/logo/logo.png",
+    3: "/images/logo/logo.png",
+    4: "/images/logo/logo.png",
+    5: "/images/logo/logo.png",
+  } as Record<number, string>,
+
+  filter: {
+    1: "drop-shadow(0 0 64px rgba(253, 149, 4, 0.5))",
+    2: "drop-shadow(0 0 64px rgba(253, 149, 4, 0.5))",
+    3: "drop-shadow(0 0 64px rgba(253, 149, 4, 0.5))",
+    4: "drop-shadow(0 0 64px rgba(253, 149, 4, 0.5))",
+    5: "drop-shadow(0 0 64px rgba(253, 149, 4, 0.5))",
+  } as Record<number, string>,
+
+  bg: {
+    1: "/images/levels/Frog-bg.png",
+    2: "/images/levels/Frog-bg.png",
+    3: "/images/levels/Frog-bg.png",
+    4: "/images/levels/Frog-bg.png",
+    5: "/images/levels/Frog-bg.png",
+  } as Record<number, string>,
+};
+
+export default levelConfig;
